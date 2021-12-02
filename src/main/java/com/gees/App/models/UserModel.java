7e2d80739b4ef69c4cf9e077eee090485d3f33ac
Gustavo Boaz <gustavo.boaz@hotmail.com>
2021-12-02 12:16:15 -0300

    @GustavoBoaz [Task: Initial commit]

---
+++ src/main/java/com/gees/App/models/UserModel.java
@@ -0,0 +1,59 @@
+package com.gees.App.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_users")
+public class UserModel {
+
+	private @Id @GeneratedValue(strategy = GenerationType.IDENTITY) Long idUser;
+	private String name;
+	private String email;
+	private String password;
+	private Long points;
+
+	public Long getIdUser() {
+		return idUser;
+	}
+
+	public void setIdUser(Long idUser) {
+		this.idUser = idUser;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Long getPoints() {
+		return points;
+	}
+
+	public void setPoints(Long points) {
+		this.points = points;
+	}
+
+}
