596eeb55b631b82628942fd69699de66fe881b1e
Rafael Borges <rafaelborges.av@gmail.com>
2021-12-03 12:02:26 -0300

    @okborges [Task: ADD documentation in UserRepository]

--- src/main/java/com/gees/App/repositories/UserRepository.java
+++ src/main/java/com/gees/App/repositories/UserRepository.java
@@ -11,8 +11,29 @@ import com.gees.App.models.UserModel;
 @Repository
 public interface UserRepository extends JpaRepository<UserModel, Long> {
 
+	/**
+	 * Método utilizado para pesquisar por email na coluna email do bando de dados
+	 * 
+	 * @param emailUser
+	 * @return Optional<UserModel>
+	 * @author BORGES
+	 * @since 1.0
+	 * 
+	 * @see findAllByNameContainingAndEmailContaining
+	 */
 	Optional<UserModel> findByEmail(String emailUser);
 	
+	/**
+	 * Método utilizado para pesquisar por emal ou name no bancode dados
+	 * 
+	 * @param name
+	 * @param email
+	 * @return List<UserModel>
+ 	 * @author BORGES
+	 * @since 1.0
+	 * 
+	 * @see findByEmail
+	 */
 	List<UserModel> findAllByNameContainingAndEmailContaining(String name, String email);
 
 }
