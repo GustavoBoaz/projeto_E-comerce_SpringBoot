f4b7556847dfe4a69f79557f1ee17d14eeea279f
Gustavo Boaz <gustavo.boaz@hotmail.com>
2021-12-09 20:10:28 -0300

    @GustavoBoaz [Task: ADD method findAllBy]

--- src/main/java/com/gees/App/repositories/ProductRepository.java
+++ src/main/java/com/gees/App/repositories/ProductRepository.java
@@ -1,6 +1,9 @@
 package com.gees.App.repositories;
 
+import java.util.List;
+
 import com.gees.App.models.ProductModel;
+import com.gees.App.util.DrinkTypes;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -16,5 +19,17 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface ProductRepository extends JpaRepository<ProductModel, Long> {
+
+    /**
+	 * Method used to search by typeProduct and product
+	 * 
+	 * @param typeProduct, DrinkTypes type
+     * @param email, String type
+	 * @return List<ProductModel>
+	 * @author Boaz
+	 * @since 1.0
+	 * 
+	 */
+    List<ProductModel> findAllByTypeProductAndProductContaining(DrinkTypes typeProduct, String product);
     
 }
