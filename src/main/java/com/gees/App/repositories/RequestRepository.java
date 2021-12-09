6ee69713f1181cc25456dfb8e5c21bba72e63f18
Gustavo Boaz <gustavo.boaz@hotmail.com>
2021-12-09 20:29:50 -0300

    @GustavoBoaz [Task: ADD repository documentation]

--- src/main/java/com/gees/App/repositories/RequestRepository.java
+++ src/main/java/com/gees/App/repositories/RequestRepository.java
@@ -5,6 +5,15 @@ import com.gees.App.models.RequestModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Inteface reponseble for inheriting crud methods
+ * 
+ * @author Boaz
+ * @since 1.0
+ * @see UserRepository
+ * @see ProductRepository
+ *
+ */
 @Repository
 public interface RequestRepository extends JpaRepository<RequestModel, Long> {
     
