c99727ac9f67483409b2567718dcb1bb78c9e243
Gustavo Boaz <gustavo.boaz@hotmail.com>
2021-12-13 17:00:40 -0300

    @GustavoBoaz [Task: MODIFY endpoint save]

--- src/main/java/com/gees/App/controllers/UserController.java
+++ src/main/java/com/gees/App/controllers/UserController.java
@@ -1,12 +1,18 @@
 package com.gees.App.controllers;
 
+import java.util.Optional;
+
 import javax.validation.Valid;
 
 import com.gees.App.models.UserModel;
+import com.gees.App.models.dtos.UserRegisterDTO;
 import com.gees.App.repositories.UserRepository;
+import com.gees.App.services.UserServices;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +20,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api/v1/user")
@@ -21,10 +28,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private @Autowired UserRepository repository;
+    private @Autowired UserServices services;
 
     @PostMapping
-    public ResponseEntity<UserModel> save(@Valid @RequestBody UserModel newUser){
-        return ResponseEntity.status(201).body(repository.save(newUser));
+    public ResponseEntity<UserModel> save(@Valid @RequestBody UserRegisterDTO newUser){
+    	return services.registerUser(newUser);
     }
 
     @GetMapping("/{id_user}")
